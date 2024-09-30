92f2d4f7825e07009cdd1b964488bf53a5f7b5f2
wwwalohacampus <h850415@naver.com>
2024-09-30 18:18:18 +0900

    🚀html css js 끝

---
+++ JS/09_AJAX/board/src/main/java/com/aloha/board/mapper/BoardMapper.java
@@ -0,0 +1,23 @@
+package com.aloha.board.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.aloha.board.dto.Board;
+
+@Mapper     // MyBatis 의 매퍼 인터페이스로 지정하는 어노테이션
+public interface BoardMapper {
+
+    // 게시글 목록 - [페이징] + [검색]
+    // public List<Board> list(Page page) throws Exception;
+    public List<Board> list() throws Exception;
+    // 게시글 조회
+    public Board select(int no) throws Exception;
+    // 게시글 등록
+    public int insert(Board board) throws Exception;
+    // 게시글 수정
+    public int update(Board board) throws Exception;
+    // 게시글 삭제
+    public int delete(int no) throws Exception;
+}
